554dbd8a410e273b59cccded8c343a6fb56156c1
cedric Giboulot <cedric Giboulot>
2016-08-12 18:28:18 +0200

    init mockito

--- myExo/order-service-Mockito/src/test/java/com/bigbox/b2csite/order/model/transformer/OrderEntityToOrderSummaryTransformerTest.java
+++ myExo/order-service-Mockito/src/test/java/com/bigbox/b2csite/order/model/transformer/OrderEntityToOrderSummaryTransformerTest.java
@@ -1,11 +1,9 @@
 package com.bigbox.b2csite.order.model.transformer;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.UUID;
 
-import org.hamcrest.core.IsNot;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -15,74 +13,63 @@ import com.bigbox.b2csite.order.model.domain.OrderSummary;
 import com.bigbox.b2csite.order.model.entity.OrderEntity;
 import com.bigbox.b2csite.order.model.entity.OrderItemEntity;
 
-import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+
 
 public class OrderEntityToOrderSummaryTransformerTest {
 
 	private OrderEntityToOrderSummaryTransformer target = null;
-
+	
 	@Before
 	public void setup() {
 		target = new OrderEntityToOrderSummaryTransformer();
 	}
-
+	
 	@Test
 	public void test_transform_success() {
-
+		
 		String orderNumberFixture = UUID.randomUUID().toString();
-
+		
 		OrderEntity orderEntityFixture = new OrderEntity();
 		orderEntityFixture.setOrderNumber(orderNumberFixture);
 		orderEntityFixture.setOrderItemList(new LinkedList<OrderItemEntity>());
-
+		
 		OrderItemEntity itemFixture1 = new OrderItemEntity();
 		itemFixture1.setQuantity(1);
-		itemFixture1.setSellingPrice(new BigDecimal(10));
+		itemFixture1.setSellingPrice(new BigDecimal("10.00"));
 		orderEntityFixture.getOrderItemList().add(itemFixture1);
-
+		
 		OrderItemEntity itemFixture2 = new OrderItemEntity();
 		itemFixture2.setQuantity(2);
-		itemFixture2.setSellingPrice(new BigDecimal(15));
+		itemFixture2.setSellingPrice(new BigDecimal("1.50"));
 		orderEntityFixture.getOrderItemList().add(itemFixture2);
-
+		
 		OrderSummary result = target.transform(orderEntityFixture);
-
-		assertThat(result, is(notNullValue()));
-		// assertThat(null, is(nullValue()));
-		assertThat(result.getOrderNumber(), is(orderNumberFixture));
-		assertThat(result.getItemCount(), is(3));
-		assertThat(result.getTotalAmount(), is(new BigDecimal(40)));
-
+		
+		Assert.assertNotNull(result);
+		
+		Assert.assertEquals(orderNumberFixture, result.getOrderNumber());
+		Assert.assertEquals(3, result.getItemCount());
+		Assert.assertEquals(new BigDecimal("13.00"), result.getTotalAmount());
 	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void test_transform_input_is_null() {
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void test_transform_inputIsNull() {
 		target.transform(null);
 	}
-
-	@Test
-	public void test_transform_no_item_in_order() {
+	
+	public void test_transform_noItemsInOrder() {
+		
 		String orderNumberFixture = UUID.randomUUID().toString();
-
+		
 		OrderEntity orderEntityFixture = new OrderEntity();
 		orderEntityFixture.setOrderNumber(orderNumberFixture);
 		orderEntityFixture.setOrderItemList(new LinkedList<OrderItemEntity>());
-
+		
 		OrderSummary result = target.transform(orderEntityFixture);
-
-		assertThat(result, is(notNullValue()));
-
-		assertThat(result.getOrderNumber(), is(orderNumberFixture));
-		assertThat(result.getItemCount(), is(0));
-		assertThat(result.getTotalAmount(), is(new BigDecimal(0)));
+		
+		Assert.assertNotNull(result);
+		
+		Assert.assertEquals(0, result.getItemCount());
+		Assert.assertEquals(new BigDecimal("13.00"), result.getTotalAmount());
 	}
-
 }
--- myExo/order-service/src/main/java/com/bigbox/b2csite/order/model/transformer/OrderEntityToOrderSummaryTransformer.java
+++ myExo/order-service/src/main/java/com/bigbox/b2csite/order/model/transformer/OrderEntityToOrderSummaryTransformer.java
@@ -1,6 +1,7 @@
 package com.bigbox.b2csite.order.model.transformer;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 import com.bigbox.b2csite.order.model.domain.OrderSummary;
 import com.bigbox.b2csite.order.model.entity.OrderEntity;
@@ -10,4 +11,31 @@ import com.bigbox.b2csite.order.model.entity.OrderItemEntity;
 
 public class OrderEntityToOrderSummaryTransformer {
 
+	
+	public OrderSummary transform(OrderEntity orderEntity){
+		
+		if(orderEntity == null){
+			throw new IllegalArgumentException(); 
+		}
+		
+		
+		OrderSummary orderSummaryResult = new OrderSummary(); 
+		
+		orderSummaryResult.setOrderNumber(orderEntity.getOrderNumber());
+		
+		int itemCount= 0; 
+		BigDecimal orderAmount = new BigDecimal(0); 
+		
+		for(OrderItemEntity order : orderEntity.getOrderItemList()) {
+			itemCount += order.getQuantity(); 
+			BigDecimal quantityBD = new BigDecimal(order.getQuantity()); 
+			BigDecimal itemTotal = order.getSellingPrice().multiply(quantityBD); 
+			orderAmount = orderAmount.add(itemTotal); 
+		}
+		orderSummaryResult.setItemCount(itemCount);
+		orderSummaryResult.setTotalAmount(orderAmount);
+		
+		
+		return orderSummaryResult; 
+	}
 }
